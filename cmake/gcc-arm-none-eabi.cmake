7b97f1aa4a1125294ba193c24bde8c3ba1bc45ec
sarath-menon <sarathmenon.downey@gmail.com>
2023-06-06 20:34:09 +0200

    added proper compilation flags

--- cmake/gcc-arm-none-eabi.cmake
+++ cmake/gcc-arm-none-eabi.cmake
@@ -34,10 +34,11 @@ set(CMAKE_OBJDUMP ${COMPILER_PREFIX}objdump)
 set(CMAKE_SIZE ${COMPILER_PREFIX}size)
 set(CMAKE_SIZE ${COMPILER_PREFIX}strip)
 
+set(CMAKE_BUILD_TYPE "MinSizeRel")
+
 # Compiler flags --------------------------------------------------------
 
-set(CMAKE_C_FLAGS
-    "${CMAKE_C_FLAGS} -Wno-psabi -fdata-sections -ffunction-sections")
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fdata-sections -ffunction-sections")
 set(CMAKE_C_FLAGS
     "${CMAKE_C_FLAGS} -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 -g3"
 )
@@ -45,9 +46,9 @@ set(CMAKE_C_FLAGS
     "${CMAKE_C_FLAGS} -fno-math-errno -DARM_MATH_CM4 -D__FPU_PRESENT=1 -mfp16-format=ieee"
 )
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wno-array-bounds -Wno-stringop-overread")
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wno-stringop-overflow -MD -MP -MF")
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wno-stringop-overflow ")
 set(CMAKE_C_FLAGS
-    "${CMAKE_C_FLAGS} -Os -Wmissing-braces -fno-strict-aliasing ${C_PROFILE} -std=gnu11 -Wdouble-promotion "
+    "${CMAKE_C_FLAGS} -Wmissing-braces -fno-strict-aliasing ${C_PROFILE}  -Wdouble-promotion "
 )
 
 # set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${CMAKE_C_FLAGS}") set(CMAKE_CXX_FLAGS
@@ -67,6 +68,7 @@ set(LINKER_FLAGS
     -lc
     -lm
     -lnosys
+    -Wl,--print-memory-usage
     # -lstdc++
     $<$<COMPILE_LANGUAGE:CXX>:-nostdinc++
     >
