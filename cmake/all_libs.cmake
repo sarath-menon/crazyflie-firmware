b620594421740a45913c62889863f21e29348854
sarath-menon <sarathmenon.downey@gmail.com>
2023-06-08 00:28:05 +0200

    moved 4 global cmake files to local folders

--- cmake/all_libs.cmake
+++ cmake/all_libs.cmake
@@ -4,11 +4,10 @@ set(LIB_DIR ${CMAKE_SOURCE_DIR}/src/lib)
 
 include(${CMAKE_CURRENT_SOURCE_DIR}/cmake/src/helper.cmake)
 
-# libraries
-include(${CMAKE_CURRENT_SOURCE_DIR}/cmake/src/deck.cmake)
-include(${CMAKE_CURRENT_SOURCE_DIR}/cmake/src/drivers.cmake)
-include(${CMAKE_CURRENT_SOURCE_DIR}/cmake/src/hal.cmake)
-include(${CMAKE_CURRENT_SOURCE_DIR}/cmake/src/lib.cmake)
+# libraries include(${CMAKE_CURRENT_SOURCE_DIR}/cmake/src/deck.cmake)
+# include(${CMAKE_CURRENT_SOURCE_DIR}/cmake/src/drivers.cmake)
+# include(${CMAKE_CURRENT_SOURCE_DIR}/cmake/src/hal.cmake)
+# include(${CMAKE_CURRENT_SOURCE_DIR}/cmake/src/lib.cmake)
 include(${CMAKE_CURRENT_SOURCE_DIR}/cmake/src/modules.cmake)
 include(${CMAKE_CURRENT_SOURCE_DIR}/cmake/src/freertos.cmake)
 include(${CMAKE_CURRENT_SOURCE_DIR}/cmake/src/stm32.cmake)
