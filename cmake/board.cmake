f49b7285d3a750461c2ecc02142a2808528822a7
sarath-menon <sarathmenon.downey@gmail.com>
2023-06-07 18:10:29 +0200

    updated linker script, startup file

--- cmake/board.cmake
+++ cmake/board.cmake
@@ -9,5 +9,6 @@ set(CPU_PARAMETERS -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16)
 
 # Scripts --------------------------------------------------------
 
-set(STARTUP_SCRIPT ${CMAKE_SOURCE_DIR}/src/init/startup_stm32f40xx.S)
-set(MCU_LINKER_SCRIPT ${CMAKE_SOURCE_DIR}/tools/make/F405/linker/FLASH_CLOAD.ld)
+set(STARTUP_SCRIPT ${CMAKE_SOURCE_DIR}/src/init/startup_stm32f405xx.s)
+set(MCU_LINKER_SCRIPT
+    ${CMAKE_SOURCE_DIR}/tools/make/F405/linker/STM32F405RGTx_FLASH.ld)
