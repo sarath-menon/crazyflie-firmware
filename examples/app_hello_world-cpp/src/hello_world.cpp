ccbcbb0776941432b42f3febed3eb3da7e7827ff
sarath-menon <sarathmenon.downey@gmail.com>
2023-06-09 01:05:39 +0200

    tested various msg sending cmd's

--- examples/app_hello_world-cpp/src/hello_world.cpp
+++ examples/app_hello_world-cpp/src/hello_world.cpp
@@ -1,6 +1,9 @@
 #define DEBUG_MODULE "HELLOWORLD"
 
 extern "C" {
+#include "cfassert.h"
+#include "console.h"
+#include "usb.h"
 #include <stdbool.h>
 #include <stdint.h>
 #include <string.h>
@@ -13,20 +16,45 @@ extern "C" {
 #include "task.h"
 }
 
+constexpr static uint8_t SYSLINK_MTU = 64;
+static uint8_t sendBuffer[64];
+
+static int usblinkSendPacket(CRTPPacket *p) {
+  int dataSize;
+
+  ASSERT(p->size < SYSLINK_MTU);
+
+  sendBuffer[0] = p->header;
+
+  if (p->size <= CRTP_MAX_DATA_SIZE) {
+    memcpy(&sendBuffer[1], p->data, p->size);
+  }
+  dataSize = p->size + 1;
+
+  // ledseqRun(&seq_linkDown);
+
+  return usbSendData(dataSize, sendBuffer);
+}
+
 void appMain() {
 
   static CRTPPacket msg;
 
   while (1) {
     vTaskDelay(M2T(1000));
-    DEBUG_PRINT("Hello World!\n");
+    // DEBUG_PRINT("Hello World!\n");
 
     uint8_t size = 1;
     msg.size = size;
 
-    uint8_t data = 'a';
+    // uint8_t data = 'h';
+    const char str[20] = {"selva\n"};
 
-    msg.data[0] = data;
-    crtpSendPacket(&msg);
+    // msg.data[0] = data;
+    // // usblinkSendPacket(&msg);
+    // crtpSendPacket(&msg);
+
+    // consolePutchar(data);
+    consolePuts(str);
   }
 }
