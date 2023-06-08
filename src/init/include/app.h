33f239c0b7bf78562706f2e82f572f5c27a2b83c
sarath-menon <sarathmenon.downey@gmail.com>
2023-06-08 11:04:58 +0200

    microdds module works

---
+++ src/init/include/app.h
@@ -0,0 +1,3 @@
+#pragma once
+
+void appMain(void *argument);
---
+++ src/init/src/app.c
@@ -0,0 +1,8 @@
+#include "app.h"
+
+/*FreeRtos includes*/
+#include "FreeRTOS.h"
+#include "task.h"
+
+#include <uxr/client/client.h>
+#include <ucdr/microcdr.h>
