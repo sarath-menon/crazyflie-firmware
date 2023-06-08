605b9e151146d6a9ed5915af867c461b8c68b741
sarath-menon <sarathmenon.downey@gmail.com>
2023-06-08 11:30:02 +0200

    added function declarations

--- src/init/include/app.h
+++ src/init/include/app.h
@@ -1,3 +1,27 @@
 #pragma once
 
-void appMain(void *argument);
+#include <device.h>
+#include <unistd.h>
+#include <uxr/client/client.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct {
+  size_t fd;
+  struct device *uart_dev;
+} dds_transport_params_t;
+
+static dds_transport_params_t default_params;
+
+bool dds_transport_open(struct uxrCustomTransport *transport);
+bool dds_transport_close(struct uxrCustomTransport *transport);
+size_t dds_transport_write(struct uxrCustomTransport *transport,
+                           const uint8_t *buf, size_t len, uint8_t *err);
+size_t dds_transport_read(struct uxrCustomTransport *transport, uint8_t *buf,
+                          size_t len, int timeout, uint8_t *err);
+
+#ifdef __cplusplus
+}
+#endif
--- src/init/src/app.c
+++ src/init/src/app.c
@@ -5,4 +5,24 @@
 #include "task.h"
 
 #include <uxr/client/client.h>
+#include <uxr/client/transport.h>
 #include <ucdr/microcdr.h>
+
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+#define RING_BUF_SIZE 2048
+
+char uart_in_buffer[RING_BUF_SIZE];
+char uart_out_buffer[RING_BUF_SIZE];
+
+bool dds_transport_open(struct uxrCustomTransport * transport){}
+
+bool dds_transport_close(struct uxrCustomTransport * transport){}
+
+size_t dds_transport_write(struct uxrCustomTransport *transport,
+                           const uint8_t *buf, size_t len, uint8_t *err){}
+
+size_t dds_transport_read(struct uxrCustomTransport* transport, uint8_t* buf, size_t len, int timeout, uint8_t* err){}
