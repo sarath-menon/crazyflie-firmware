bb6a68b653dfdd834adbfac4283e88d19465ee44
Sarath S Menon <sarathmenon.downey@gmail.com>
2024-05-07 16:16:07 +0200

    added basic math lib

--- examples/rls_controller/src/math_lib.h
+++ examples/rls_controller/src/math_lib.h
@@ -2,16 +2,19 @@
 #define MATRIX_OPERATIONS_H
 
 // Define the Matrix structure
-typedef struct {
-    double* data;
+typedef struct
+{
+    double *data;
     int rows;
     int cols;
 } Matrix;
 
 // Function prototypes for matrix operations
-void matMul(Matrix* result, Matrix* a, Matrix* b);
-void matSub(Matrix* result, Matrix* a, Matrix* b);
-void matCopy(Matrix* dest, Matrix* src);
-void getSubMatrix(Matrix* result, Matrix* src, int* indices, int num_indices);
+void matMul(Matrix *result, Matrix *a, Matrix *b);
+void matSub(Matrix *result, Matrix *a, Matrix *b);
+void matCopy(Matrix *dest, Matrix *src);
+void getSubMatrix(Matrix *result, Matrix *src, int *indices, int num_indices);
+
+void create_identity_matrix(float *matrix, int size);
 
 #endif // MATRIX_OPERATIONS_H
